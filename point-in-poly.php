5576e3f939014f7d66fff35076fa42e677b7ca5a
Phil Birnie <me@philbirnie.com>
2016-01-09 14:37:14 -0500

    Adds KMZ and updates point-in-poly for testing.

--- point-in-poly.php
+++ point-in-poly.php
@@ -2,7 +2,7 @@
 
 include_once('library/autoloader.php');
 
-$point = new \AEP\Point(-82.55, 39.26);
+$point = new \AEP\Point(-83.3203125, 38.285624966683756);
 $xml = new \AEP\KML\Parser(__DIR__ . '/assets/kml/east.kml');
 
 $aep = new \AEP\AEP($point, $xml);
